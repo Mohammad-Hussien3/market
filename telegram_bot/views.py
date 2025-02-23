d7e9747377ed56013027172c5c435b7e48ba673a
Mohammad-Hussien3 <mohammad.hussien121@gmail.com>
2025-02-23 11:42:30 +0300

    edit menu

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -31,11 +31,13 @@ class Webhook(APIView):
 
             if referrer_id != None:
                 text = args[0]
-                if profile.referred_by == None:
-                    profile.referred_by = referrer_id
-                    referred_profile = Profile.objects.get(telegram_id=referrer_id)
-                    referred_profile.points += 10
-                    referred_profile.save()
+                referrer_id = int(referrer_id)
+                if referrer_id != chat_id:
+                    if profile.referred_by == None:
+                        profile.referred_by = referrer_id
+                        referred_profile = Profile.objects.get(telegram_id=referrer_id)
+                        referred_profile.points += 10
+                        referred_profile.save()
 
             if 'first_name' in data['message']['chat']:
                 profile.first_name = data['message']['chat']['first_name']
@@ -48,52 +50,64 @@ class Webhook(APIView):
 
             profile.save()
 
-            if text == "/start":
+            if text == '/start':
                 self.send_welcome_message(chat_id)
                 self.send_menu(chat_id)
 
-        elif "callback_query" in data:
-            callback_data = data["callback_query"]["data"]
-            chat_id = data["callback_query"]["message"]["chat"]["id"]
+        elif 'callback_query' in data:
+            callback_data = data['callback_query']['data']
+            chat_id = data['callback_query']['message']['chat']['id']
             self.handle_callback(chat_id, callback_data)
 
         return JsonResponse({'status': 'ok'})
     
     def send_welcome_message(self, chat_id):
-        message = "مرحبًا بك في البوت! 😊\nاستخدم الأوامر التالية للتفاعل معي:"
+        message = 'مرحبًا بك في البوت! 😊\nاستخدم الأوامر التالية للتفاعل معي:'
         payload = {
-            "chat_id": chat_id,
-            "text": message
+            'chat_id': chat_id,
+            'text': message
         }
         requests.post(TELEGRAM_API_URL, json=payload)
 
     def send_menu(self, chat_id):
         commands = [
-            [{"text": "🛍️ زيارة المتجر للشراء", "web_app" : {"url": WEB_APP_URL}}],
-            [{"text": "📍 عرض نقاطي", "callback_data": "points"}],
-            [{"text": "📢 احصل على رابط الإحالة", "callback_data": "referral"}],
-            [{"text": "📌 عرض الطلبات المعلقة", "callback_data": "pending_orders"}],
-            [{"text": "🛒 عرض طلباتي السابقة", "callback_data": "my_orders"}],
+            [{'text': '🛍️ زيارة المتجر للشراء', 'web_app' : {'url': WEB_APP_URL}}],
+            [{'text': '📍 عرض نقاطي', 'callback_data': 'points'}],
+            [{'text': '📢 احصل على رابط الإحالة', 'callback_data': 'referral'}],
+            [{'text': '📌 عرض الطلبات المعلقة', 'callback_data': 'pending_orders'}],
+            [{'text': '🛒 عرض طلباتي السابقة', 'callback_data': 'my_orders'}],
         ]
 
         payload = {
-            "chat_id": chat_id,
-            "text": "اختر أحد الخيارات:",
-            "reply_markup": {"inline_keyboard": commands}
+            'chat_id': chat_id,
+            'text': 'اختر أحد الخيارات:',
+            'reply_markup': {'inline_keyboard': commands}
         }
         requests.post(TELEGRAM_API_URL, json=payload)
     
     def handle_callback(self, chat_id, callback_data):
-        if callback_data == "points":
+        if callback_data == 'points':
             self.send_points(chat_id)
+        elif callback_data == 'referral':
+            self.send_referral(chat_id)
 
     def send_points(self, chat_id):
         profile = Profile.objects.get(telegram_id=chat_id)
-        message = f"📍 لديك {profile.points} نقاط."
+        message = f'📍 لديك {profile.points} نقاط.'
 
         payload = {
-            "chat_id": chat_id,
-            "text": message
+            'chat_id': chat_id,
+            'text': message
+        }
+        requests.post(TELEGRAM_API_URL, json=payload)
+    
+    def send_referral(self, chat_id):
+        profile = Profile.objects.get(telegram_id=chat_id)
+        message = f'رابط احالتك هوة:{profile.referral_link}.'
+
+        payload = {
+            'chat_id': chat_id,
+            'text': message
         }
         requests.post(TELEGRAM_API_URL, json=payload)
 

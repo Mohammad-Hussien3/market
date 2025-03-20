36448e8e3b9cca95ea34dc9e34f2ebc5ba190e72
Mohammad-Hussien3 <mohammad.hussien121@gmail.com>
2025-03-20 21:23:26 +0300

    edit token

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -12,7 +12,7 @@ from rest_framework.views import Response, status
 from item.models import Order
 
 
-BOT_TOKEN = "7840572100:AAE49wkw2i04ZwbGMVwOFuBK4qmNFCHyVDY"
+BOT_TOKEN = "7706720810:AAHtk9RCd9nKr4a0nNWNPr2zhh4dOJE3SaQ"
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 WEB_APP_URL = "https://nehad223.github.io/dsad/#/dsad/"
 

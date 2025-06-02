616346afff10e9010a423f8b69ca1d160580adb4
Mohammad-Hussien3 <mohammad.hussien121@gmail.com>
2025-06-03 00:03:36 +0300

    edit

--- usermanagament/views.py
+++ usermanagament/views.py
@@ -1,13 +1,21 @@
 from usermanagament.models import Admin
-from rest_framework.views import APIView
-from usermanagament.serializers import AdminSerializer
+from rest_framework.views import APIView, Response, status
 
+class UpdatePassword(APIView):
 
-# class UpdatePassword(APIView):
-
-#     def post(self, request):
-#         data = request.data
-#         old_password = data['old_password']
-#         new_password = data['new_password']
-#         if Admin.admin_password == old_password or Admin.sub_admin_password == old_password:
+    def put(self, request):
+        data = request.data
+        old_password = data['old_password']
+        new_password = data['new_password']
+        if Admin.admin_password == old_password or Admin.sub_admin_password == old_password:
+            if Admin.admin_password == old_password:
+                Admin.admin_password = new_password
+                Admin.save()
+            else:
+                Admin.sub_admin_password = new_password
+                Admin.save()
             
+            return Response({'success':'success'}, status=status.HTTP_200_OK)
+        
+        else:
+            return Response({'error':'error'}, status=status.HTTP_404_NOT_FOUND)
